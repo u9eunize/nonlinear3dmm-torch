Move shape1 calculation form helper to model. Fix save and load errors. Add image log for training. Fix weight update errors. Fix global step index errors. Modulated train step function (network, rendering, loss).

network/Nonlinear_3DMM.py
@@ -42,18 +42,11 @@ class Nonlinear3DMM(nn.Module):
         self.w_shape = torch.tensor(w_shape, dtype=dtype)
         self.w_exp = torch.tensor(w_exp, dtype=dtype)
 
+        # generate shape1d
+        self.vt2pixel_u, self.vt2pixel_v = load_3DMM_vt2pixel()
 
-        # encoder
-        '''
-        self.nl_encoder = NLEncoderBlock(self.nz, self.gf_dim)
-        self.in_dim = self.nl_encoder.in_dim
-
-        # embedding each component (camera, illustration, shape, texture)
-        self.lv_m_layer = NLEmbeddingBlock(self.in_dim, self.gfc_dim // 5, fc_dim=self.m_dim)
-        self.lv_il_layer = NLEmbeddingBlock(self.in_dim, self.gfc_dim // 5, fc_dim=self.il_dim)
-        self.lv_shape_layer = NLEmbeddingBlock(self.in_dim, self.gfc_dim // 2)
-        self.lv_tex_layer = NLEmbeddingBlock(self.in_dim, self.gfc_dim // 2)
-        '''
+        self.vt2pixel_u = torch.tensor(self.vt2pixel_u[:-1], dtype=dtype)
+        self.vt2pixel_v = torch.tensor(self.vt2pixel_v[:-1], dtype=dtype)
 
         ###################################### encoder shasha
         self.nl_encoder = Encoder(self.nz, self.gf_dim, self.gfc_dim // 5, self.gfc_dim // 5, self.gfc_dim // 2, self.gfc_dim // 2, self.m_dim, self.il_dim)
@@ -63,26 +56,27 @@ class Nonlinear3DMM(nn.Module):
         self.shape_gen = NLShapeDecoderBlock(self.gfc_dim//2, self.gf_dim, self.gfc_dim, self.tex_sz)
 
     def forward(self, input_images):
-        '''
-        # encoding
-        encoder_out = self.nl_encoder(input_images)
+        batch_size = input_images.shape[0]
 
-        # latent vector embedding m, il, shape, tex
-        lv_m = self.lv_m_layer(encoder_out)
-        lv_il = self.lv_il_layer(encoder_out)
-        lv_shape = self.lv_shape_layer(encoder_out)
-        lv_tex = self.lv_tex_layer(encoder_out)
-        '''
         lv_m, lv_il, lv_shape, lv_tex = self.nl_encoder(input_images)
 
         # generate albedo and shape
         albedo = self.albedo_gen(lv_tex)
         shape2d = self.shape_gen(lv_shape)
 
-        return lv_m, lv_il, lv_shape, lv_tex, albedo, shape2d
+        vt2pixel_u = self.vt2pixel_u.view((1, 1, -1)).repeat(batch_size, 1, 1)
+        vt2pixel_v = self.vt2pixel_v.view((1, 1, -1)).repeat(batch_size, 1, 1)
+
+        shape1d = bilinear_sampler_torch(shape2d, vt2pixel_u, vt2pixel_v)
+        shape1d = shape1d.view(batch_size, -1)
+
+        return lv_m, lv_il, lv_shape, lv_tex, albedo, shape2d, shape1d
 
     def to(self, device, *args, **kwargs):
         ret = super(Nonlinear3DMM, self).to(device, *args, **kwargs)
+
+        self.vt2pixel_u = self.vt2pixel_u.to(device)
+        self.vt2pixel_v = self.vt2pixel_v.to(device)
 
         self.mean_shape = self.mean_shape.to(device)
         self.std_shape = self.std_shape.to(device)