d0df38a511901507fe7ac61cd5eaca4051425f10
Woogeun Bae <woogeun_bae@tmax.co.kr>
2020-10-30 09:22:08 +0900

    added redner operation for taehwan dataset

--- renderer/rendering_example.py
+++ renderer/rendering_example.py
@@ -1,162 +1,65 @@
-from network.Nonlinear_3DMM import Nonlinear3DMM
-from loss import Loss
-from os.path import join, basename
-from glob import glob
-import torch
-import torchvision.transforms.functional as F
-import torchvision
-from PIL import Image
 import pyredner
-import redner
-from renderer.rendering_ops import *
-from settings import CFG
-from configure_dataset import NonlinearDataset
-
-
-dtype = torch.float32
-mu_shape, w_shape = load_Basel_basic('shape')
-mu_exp, w_exp = load_Basel_basic('exp')
-mean_shape = torch.tensor(mu_shape + mu_exp, dtype=dtype).to(CFG.device)
-std_shape = torch.tensor(np.tile(np.array([1e4, 1e4, 1e4]), CFG.vertex_num), dtype=dtype).to(CFG.device)
-mean_m = torch.tensor(np.load(join(CFG.dataset_path, 'mean_m.npy')), dtype=dtype).to(CFG.device)
-std_m = torch.tensor(np.load(join(CFG.dataset_path, 'std_m.npy')), dtype=dtype).to(CFG.device)
-def save_to_obj(name, vertex, face):
-   with open(name, 'w') as fd:
-      result = ""
-      for v1, v2, v3 in vertex:
-         result += f'v {v1:.3f} {v2:.3f} {v3:.3f}\n'
-      result += "\n"
-      for f1, f2, f3 in face:
-         result += f'f {f3 + 1} {f2 + 1} {f1 + 1}\n'
-      fd.write(result)
-   print(name)
-def compute_uvs(texture, m, shape):
-   u, v = warp_texture_torch(texture, m, shape, with_uv=True)
-   return torch.tensor(torch.cat([u[:-1], v[:-1]]).view(-1,2), dtype=torch.float32)
-def main():
-   tri = load_3DMM_tri()
-   face = np.transpose(tri)[:-1]
-   face = np.stack([face[:, 0], face[:, 2], face[:, 1]], axis=1)
-   # checkerboard_texture = pyredner.imread('checkerboard.exr')
-   # define model and loss
-   model = Nonlinear3DMM().to(CFG.device)
-   model, _, _, _, _ = load(model)
-   # load images for prediction
-   fnames_raw = glob(join(CFG.prediction_src_path, "*"))
-   total_len = len(fnames_raw)
-   fnames = [fnames_raw[i:i + CFG.batch_size] for i in range(0, len(fnames_raw), CFG.batch_size)]
-   # output image lists
-   output_gt = []
-   output_images = []
-   output_images_with_mask = []
-   output_shapes = []
-   for batch_fnames in fnames:
-      input_images = []
-      # resize and crop images
-      for fname in batch_fnames:
-         img = Image.open(fname)
-         img = torchvision.transforms.functional.resize(img, CFG.image_size)
-         img = torchvision.transforms.functional.center_crop(img, CFG.image_size)
-         img = torchvision.transforms.functional.to_tensor(img)
-         input_images.append(img)
-      input_images = torch.stack(input_images, dim=0).to(CFG.device)
-      # forward network
-      with torch.no_grad():
-         result = model(input_images)
-         lv_m = result['lv_m']
-         lv_il = result['lv_il']
-         shape1d = result['shape_1d_comb']
-         albedo = result['albedo_comb']
-
-      vt2pixel_u, vt2pixel_v = load_FaceAlignment_vt2pixel(False)
-
-      vt2pixel_u = torch.from_numpy(vt2pixel_u).cuda()[:-1]
-      vt2pixel_v = torch.from_numpy(vt2pixel_v).cuda()[:-1]
-
-      vt2pixel_u = torch.clamp(vt2pixel_u, 0, 192.0) / 192
-      vt2pixel_v = torch.clamp(vt2pixel_v, 0, 224.0) / 224
-
-      # vt2pixel_u -= torch.min(vt2pixel_u)
-      # vt2pixel_u /= torch.max(vt2pixel_u)
-      #
-      # vt2pixel_v -= torch.min(vt2pixel_v)
-      # vt2pixel_v /= torch.max(vt2pixel_v)
-
-      uvs = torch.stack([vt2pixel_v, vt2pixel_u], dim=-1).float()
-
-
-      # make full
-      m_full = lv_m * std_m + mean_m
-      shape_full = shape1d * std_shape + mean_shape
-      shade = generate_shade(lv_il, m_full, shape_full)
-      tex = 2.0 * ((albedo + 1.0) / 2.0 * shade) - 1.0
-      g_images_raw, g_images_mask_raw = warp_texture_torch(tex, m_full, shape_full)
-      g_images_mask_raw = g_images_mask_raw.unsqueeze(1).repeat(1, 3, 1, 1)
-      g_images = g_images_raw * g_images_mask_raw + input_images * (torch.ones_like(g_images_mask_raw) - g_images_mask_raw)
-      output_gt += input_images
-      output_images += g_images_raw
-      output_images_with_mask += g_images
-      output_shapes += shape_full.view(shape_full.shape[0], -1, 3) / 10000
-      for m , sh, a, t, n , s in zip(m_full, output_shapes, albedo, tex,fnames_raw, shade):
-
-
-         # #############################
-         # mat = pyredner.Material(diffuse_reflectance=t.permute(1,2,0), two_sided = True)
-         # mat_black = pyredner.Material( diffuse_reflectance=torch.tensor([1.0, 1.0, 1.0], device=pyredner.get_device()))
-         # materials = [mat, mat_black]
-         # # mat = pyredner.Material()
-         # pshape = pyredner.Shape(vertices=sh, indices=torch.tensor(face, dtype=torch.int32), material_id=0, uvs = uvs)
-         # # pshape.normals = -pyredner.compute_vertex_normal(pshape.vertices.cpu(), pshape.indices.cpu()) # 필
-         # pyredner.imwrite(t.permute(1,2,0).cpu(), join(CFG.prediction_dst_path, basename(n).split('.')[0] + '_img.jpg'))
-         # z_coord = 500000
-         # light_position = 150000.0
-         # light_vertices = torch.tensor([[-light_position, -light_position, z_coord], [light_position, -light_position, z_coord], [-light_position, light_position, z_coord], [light_position, light_position, z_coord]],
-         #                        device=pyredner.get_device())
-         # light_indices = torch.tensor([[0, 1, 2], [1, 2, 3]], dtype=torch.int32, device=pyredner.get_device())
-         # shape_light = pyredner.Shape(light_vertices, light_indices, 1)
-         # camera0 = pyredner.automatic_camera_placement([pshape], resolution=(224, 224))
-         # t = camera0.position
-         # camera0.position = camera0.position - t - t
-         # # camera0.look_at = camera0.look_at - t - t
-         # # camera0.look_at = camera0.position - camera0.look_at
-         # camera = pyredner.Camera(position=camera0.position + torch.tensor([0, 0, 10]),
-         #                    look_at=camera0.look_at + torch.tensor([0, 0, 0]),
-         #                    up=camera0.up + torch.tensor([0, 0, 0]),
-         #                    fov=camera0.fov,
-         #                    resolution=camera0.resolution)
-         # light = pyredner.AreaLight(1, torch.tensor([5.0, 5.0, 5.0]), two_sided=True)
-         # scene = pyredner.Scene(camera, [pshape, shape_light], materials, [light])
-         # args = pyredner.RenderFunction.serialize_scene(scene=scene,num_samples=128,max_bounces=16)
-         # # Alias of the render function
-         # render = pyredner.RenderFunction.apply
-         # # render = pyredner.render_pathtracing(*args)
-         # img = render(0, *args)
-         # pyredner.imwrite(img.cpu(), join(CFG.prediction_dst_path, basename(n).split('.')[0] + '_redner.png'))
+import torch
+from plyfile import PlyData
+import math
+import numpy as np
+from renderer.rendering_ops_redner import render
 
 
 
+def main ():
+    # read ply data and split vertex and color data
+    # plydata = PlyData.read('1.ply')
+    # fname = '1_0.2776017385292788_0.16254296733069473_70.79018321602928.jpg'
+    plydata = PlyData.read('2.ply')
+    fname = '2_0.15897386037903233_0.2043752904048205_58.26357616953039.jpg'
+    # plydata = PlyData.read('3.ply')
+    # fname = '3_5.584806478789926_0.013549064110521272_66.73159275208857.jpg'
+    x, y, z = plydata['vertex']['x'], plydata['vertex']['y'], plydata['vertex']['z']
+    r, g, b = plydata['vertex']['red'], plydata['vertex']['green'], plydata['vertex']['blue']
+    vertex = np.stack([x, y, z], axis=1)
+    color = np.stack([r, g, b], axis=1)
+    face = np.vstack(plydata['face']['vertex_indices'])
+    
+    # define 3d model
+    vertex = torch.tensor(vertex, device=pyredner.get_device())
+    indices = torch.tensor(face, device=pyredner.get_device())
+    color = torch.tensor(color, device=pyredner.get_device()) / 255.0
 
-         ######################################################### deffered test
-         mat = pyredner.Material(diffuse_reflectance=t.permute(1, 2, 0) * 2, two_sided=True)
-         pshape = pyredner.Shape(vertices=sh, indices=torch.tensor(face, dtype=torch.int32), material_id=0, uvs=uvs)
-         pyredner.imwrite(t.permute(1, 2, 0).cpu(), join(CFG.prediction_dst_path, basename(n).split('.')[0] + '_img.jpg'))
+    # set camera parameters
+    id, pi_camera, theta_camera, rho_camera = [float(t) for t in fname.split('.jpg')[0].split('_')]
+    camera_param = torch.tensor([theta_camera, pi_camera, rho_camera], device=pyredner.get_device())
+    
+    # set light parameters
+    theta_light = math.pi * -0.5
+    pi_light = 0
+    rho_light = 10
+    intensity = 5000.0
+    light_param = torch.tensor([theta_light, pi_light, rho_light, intensity], device=pyredner.get_device())
 
-         camera0 = pyredner.automatic_camera_placement([pshape], resolution=(224, 224))
-         camera0.position = -camera0.position
-         camera = pyredner.Camera(position=camera0.position + torch.tensor([0, 0, 10]),
-                            look_at=camera0.look_at + torch.tensor([0, 0, 0]),
-                            up=camera0.up + torch.tensor([0, 0, 0]),
-                            fov=camera0.fov,
-                            resolution=camera0.resolution)
-
-         light = pyredner.PointLight(
-             position=(camera0.position + torch.tensor((0.0, 0.0, 20.0))).to(pyredner.get_device()),
-             intensity=torch.tensor((1000.0, 1000.0, 1000.0), device=pyredner.get_device()))
-         scene = pyredner.pyredner.Scene(shapes = [pshape], materials=[mat], camera = camera)
-         img = pyredner.render_deferred(scene, lights=[light])
-         pyredner.imwrite(img.cpu(), join(CFG.prediction_dst_path, basename(n).split('.')[0] + '_redner.png'))
-         # save_images(torch.pow(img, 1.0/2.2).cpu(), [4, -1], join(CFG.prediction_dst_path, basename(n).split('.')[0] + '_redner.png'))
+    # render
+    def batch_wise(tensor, batch_size):
+        dim = len(tensor.shape)
+        args = [batch_size] + [1] * dim
+        return torch.unsqueeze(tensor, dim=0).repeat(*args)
+    
+    batch_size = 2
+    
+    pyredner.set_print_timing(False)
+    g_buffer = render(vertex_batch=batch_wise(vertex, batch_size),
+                      indices_batch=batch_wise(indices, batch_size),
+                      color_batch=batch_wise(color, batch_size),
+                      camera_batch=batch_wise(camera_param, batch_size),
+                      light_batch=batch_wise(light_param, batch_size),
+                      resolution=(224,224),
+                      print_timing=True)
+    
+    # write images
+    for idx, img in enumerate(g_buffer):
+        pyredner.imwrite(img.cpu(), 'taehwan_redner_{}.png'.format(idx))
+    
+    
 
 
 if __name__ == '__main__':
-   main()
+    main()
---
+++ renderer/rendering_ops_redner.py
@@ -0,0 +1,135 @@
+import pyredner
+import torch
+import math
+import redner
+from typing import Optional, Tuple, Any
+
+
+def sphere2xyz ( theta, pi, rho ):
+    return torch.tensor(
+        [rho * math.sin(theta) * math.cos(pi), rho * math.sin(theta) * math.sin(pi), rho * math.cos(theta)],
+        device=pyredner.get_device())
+
+
+class BatchRenderFunction(torch.autograd.Function):
+    @staticmethod
+    def forward ( ctx, seed, *args ):
+        batch_dims = args[0]
+        args_old_format = args[1:]
+        chunk_len = int(len(args_old_format) / batch_dims)
+        h, w = args_old_format[12]
+        result = torch.zeros(
+            batch_dims, h, w, 3, device=pyredner.get_device(), requires_grad=True)
+        for k in range(0, batch_dims):
+            sub_args = args_old_format[k * chunk_len:(k + 1) * chunk_len]
+            result[k, :, :, :] = pyredner.RenderFunction.forward(ctx, seed, *sub_args)
+        return result
+    
+    @staticmethod
+    def backward ( ctx, grad_img ):
+        # None gradient for seed and batch_dims
+        ret_list = (None, None,)
+        batch_dims = grad_img.shape[0]
+        for k in range(0, batch_dims):
+            # [1:] cuz original backward function returns None grad for seed input, but we manage that ourselves
+            ret_list = ret_list + pyredner.RenderFunction.backward(ctx, grad_img[k, :, :, :])[1:]
+        return ret_list
+
+
+# declare constant values
+device = pyredner.get_device()
+mat = pyredner.Material(diffuse_reflectance=torch.zeros([1, 1, 3], device=device), use_vertex_color=True)
+args = {
+        'num_samples'                : (64, 16),
+        'max_bounces'                : 1,
+        'channels'                   : [redner.channels.radiance],
+        'sampler_type'               : pyredner.sampler_type.sobol,
+        'sample_pixel_center'        : False,
+        'use_primary_edge_sampling'  : True,
+        'use_secondary_edge_sampling': True,
+        'device'                     : device
+}
+batch_render = BatchRenderFunction.apply
+envmap_texels = 0.5 * torch.ones([1, 1, 3], device=device, requires_grad=True)
+envmap = pyredner.EnvironmentMap(torch.abs(envmap_texels))
+
+
+
+def render ( vertex_batch: Optional[torch.Tensor],
+             indices_batch: Optional[torch.Tensor],
+             color_batch: Optional[torch.Tensor],
+             camera_batch: Optional[torch.Tensor],
+             light_batch: Optional[torch.Tensor],
+             resolution: Tuple[int, int] = (224, 224),
+             print_timing: Any = False):
+    """
+        render batch 3D objects using path tracing mode(physically based mode)
+
+        Args
+        ====
+        vertex: Optional[torch.Tensor]
+            3D position of vertices
+            float32 tensor with size [batch_size, num_vertices, 3]
+        indices: Optional[torch.Tensor]
+            vertex indices of triangle faces.
+            int32 tensor with size [batch_size, num_triangles, 3]
+        color: Optional[torch.Tensor]
+            per-vertex color
+            float32 tensor with size [batch_size, num_vertices, 3]
+        camera_param: Optional[torch.Tensor]
+            camera parameters defined by sphere coordinates.
+            float32 tensor with size 3, [batch_size, theta, pi, rho]
+        light_param: Optional[torch.Tensor]
+            light parameters defined by sphere coordinates with intensity.
+            float32 tensor with size 3, [batch_size, theta, pi, rho, intensity]
+        resolution: Tuple[int, int]
+            resolution parameter
+            int32 tuple with size 2, [h, w]
+
+        Returns
+        =======
+        torch.Tensor
+            a camera that can see all the objects.
+            float32 tensor with size [batch_size, h, w, 3]
+    """
+    pyredner.set_print_timing(print_timing)
+    
+    batch_size = vertex_batch.shape[0]
+    scene_args = []
+    for vertex, indices, color, camera_param, light_param in zip(vertex_batch, indices_batch, color_batch, camera_batch,
+                                                                 light_batch):
+        # define face shape
+        face = pyredner.Object(vertices=vertex, indices=indices, colors=color, material=mat)
+        face.normals = pyredner.compute_vertex_normal(vertices=face.vertices, indices=face.indices)
+        
+        # camera parameters
+        theta_camera, pi_camera, rho_camera = camera_param
+        rho_camera /= 12
+        camera_position = sphere2xyz(theta_camera, pi_camera, rho_camera)
+        camera_look_at = torch.tensor([0., 0., 0.], device=device)
+        camera_up = torch.tensor([0., 1., 0.], device=device)
+        camera_fov = torch.tensor([45.], device=device)
+        camera = pyredner.Camera(position=camera_position,
+                                 look_at=camera_look_at,
+                                 up=camera_up,
+                                 fov=camera_fov,
+                                 resolution=resolution)
+        
+        # define light object
+        theta_light, pi_light, rho_light, light_intensity = light_param
+        light_position = sphere2xyz(theta_light, pi_light, rho_light)
+        light = pyredner.generate_quad_light(position=light_position,
+                                             look_at=torch.zeros(3, device=device),
+                                             size=torch.tensor([0.1, 0.1], device=device),
+                                             intensity=torch.tensor([1.0, 1.0, 1.0], device=device) * light_intensity)
+        
+        # define scene parameter
+        scene = pyredner.Scene(objects=[face, light], camera=camera, envmap=envmap)
+        scene_args += pyredner.RenderFunction.serialize_scene(scene=scene, **args)
+    
+    scene_args = [batch_size] + scene_args
+    g_buffer = batch_render(0, *scene_args)
+    
+    return g_buffer
+
+
