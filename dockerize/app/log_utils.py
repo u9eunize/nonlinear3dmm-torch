25422c2461f065a35ea72007d75ee23e816ede09
woogeun Bae <woogeun_bae@tmax.co.kr>
2021-01-17 09:58:53 +0900

    removed expression smoothness

--- dockerize/app/log_utils.py
+++ dockerize/app/log_utils.py
@@ -198,7 +198,7 @@ class NLLogger:
         ], interval=interval)
         self._write_loss_images("g_albedo", all_dict, [
             "albedo_2d_base",
-            "albedo_2d_comb",
+            "exp_2d",
         ])
         # self._write_loss_images("g_image_exp", all_dict, [
         #     "base_exp_raw",
