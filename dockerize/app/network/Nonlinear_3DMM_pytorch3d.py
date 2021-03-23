ef04d926990c2e9b59e9a7fd61848e0f88d5a0be
woogeun Bae <woogeun_bae@tmax.co.kr>
2021-03-23 15:20:07 +0900

    batch 10

--- dockerize/app/network/Nonlinear_3DMM_pytorch3d.py
+++ dockerize/app/network/Nonlinear_3DMM_pytorch3d.py
@@ -29,15 +29,15 @@ class Nonlinear3DMM_pytorch3d(nn.Module):
         self.in_dim = self.nl_encoder.in_dim
 
         self.albedo_dec = NLDecoderBlock(self.gfc_dim // 2, self.gf_dim, self.gf_dim, self.tex_sz)
-        self.albedo_gen_base = NLDecoderTailBlock(self.gf_dim, self.nz, self.gf_dim, additional_layer=True)
-        self.albedo_gen_comb = NLDecoderTailBlock(self.gf_dim, self.nz, self.gf_dim, additional_layer=True)
+        self.albedo_gen_base = NLDecoderTailBlock(self.gf_dim, self.nz, self.gf_dim)
+        self.albedo_gen_comb = NLDecoderTailBlock(self.gf_dim, self.nz, self.gf_dim)
 
         self.shape_dec = NLDecoderBlock(self.gfc_dim // 2, self.gf_dim, self.gfc_dim, self.tex_sz)
-        self.shape_gen_base = NLDecoderTailBlock(self.gf_dim, self.nz, self.gf_dim, additional_layer=True)
-        self.shape_gen_comb = NLDecoderTailBlock(self.gf_dim, self.nz, self.gf_dim, additional_layer=True)
+        self.shape_gen_base = NLDecoderTailBlock(self.gf_dim, self.nz, self.gf_dim)
+        self.shape_gen_comb = NLDecoderTailBlock(self.gf_dim, self.nz, self.gf_dim)
 
         self.exp_dec = NLDecoderBlock(self.gfc_dim // 2, self.gf_dim, self.gfc_dim, self.tex_sz)
-        self.exp_gen = NLDecoderTailBlock(self.gf_dim, self.nz, self.gf_dim, additional_layer=True)
+        self.exp_gen = NLDecoderTailBlock(self.gf_dim, self.nz, self.gf_dim)
 
     def forward(self, input_images):
         batch_size = input_images.shape[0]
